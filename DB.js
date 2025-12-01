ecadaa2cbf8a263488917ee31e3c36c45d2d5c4f
euebie <2zgv9kp7jt@privaterelay.appleid.com>
2025-12-01 14:13:13 +0900

    Update DB.js

--- DB.js
+++ DB.js
@@ -5,6 +5,6 @@ const songs = [
 
       // ✅ ㄱ
       { artist: "가내수공업", title: "오빠 뭐해요?", inst: "링크", note: "유료:200개", genre: ["kpop","pay"] },
-      { artist: "가비엔제이", title: "낮과 밤", inst: "링크", note: "", genre: ["kpop"] },
+      { artist: "가비엔제이(Gavy NJ​​​​​​​)", title: "낮과 밤(LOVE ALL)", inst: "링크", note: "", genre: ["kpop"] },
 
 ];
