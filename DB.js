d04fe288d25251d29aab1d9c6479abdcbb07fa71
euebie <2zgv9kp7jt@privaterelay.appleid.com>
2025-12-01 17:36:14 +0900

    Update DB.js

--- DB.js
+++ DB.js
@@ -16,6 +16,6 @@ const songs = [
       { artist: "강아윤", title: "끝났다는 것은 다시 시작된다는 것을", inst: "https://youtu.be/LtKvHQAnb5I", key: "0", note: "", genre: ["kpop"] },
       { artist: "강인원, 권인하, 김현식", title: "비오는 날의 수채화", inst: "https://youtu.be/fbW1A1uGQ4M", key: "+6", note: "", genre: ["kpop"] },
       { artist: "강지(GangZi)", title: "봄으로 남아줄래?", inst: "", key: "0", note: "", genre: ["kpop"] },
-      { artist: "강현수(브이원(V.One))", title: "그런가봐요", inst: "", key: "+6(이미 올림)", note: "", genre: ["kpop"] },
+      { artist: "강현수(브이원(V.One))", title: "그런가봐요", inst: "", key: "+6(키 조정됨)", note: "", genre: ["kpop"] },
 
 ];
