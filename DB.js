390097f1f903ea9a77de7251094d443818809ac7
euebie <2zgv9kp7jt@privaterelay.appleid.com>
2025-12-01 15:45:46 +0900

    Update DB.js

--- DB.js
+++ DB.js
@@ -7,4 +7,5 @@ const songs = [
       { artist: "가비엔제이(Gavy NJ)", title: "낮과 밤(LOVE ALL)", inst: "", key: "0", note: "", genre: ["kpop"] },
       { artist: "가비엔제이(Gavy NJ)", title: "해바라기", inst: "", key: "0", note: "", genre: ["kpop"] },
       { artist: "가을방학", title: "가끔 미치도록 네가 안고 싶어질 때가 있어", inst: "https://youtu.be/jeBi2T88UhU", key: "0", note: "", genre: ["kpop"] },
+      { artist: "간과쓸개", title: "그대 떠나 없는 거리", inst: "https://youtu.be/uCAvxNdJaTM", key: "0", note: "", genre: ["kpop"] },
 ];
