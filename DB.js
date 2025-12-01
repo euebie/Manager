2aa272c3cfcf3f22b3999d0a41df9f8b379ee31d
euebie <2zgv9kp7jt@privaterelay.appleid.com>
2025-12-01 14:14:48 +0900

    Update DB.js

--- DB.js
+++ DB.js
@@ -1,7 +1,6 @@
-// ✅ songs.js
-// ✅ 노래 리스트만 모아둔 파일
+// ✅ DB
 
-const songs = [
+const DB = [
 
       // ✅ ㄱ
       { artist: "가내수공업", title: "오빠 뭐해요?", inst: "링크", note: "유료:200개", genre: ["kpop","pay"] },
