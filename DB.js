a92e86cfa3349a070607b3706a9d5680d9e5f14c
euebie <2zgv9kp7jt@privaterelay.appleid.com>
2025-12-01 15:41:02 +0900

    Update DB.js

--- DB.js
+++ DB.js
@@ -6,4 +6,5 @@ const songs = [
       { artist: "가내수공업", title: "오빠 뭐해요?", inst: "https://youtu.be/aQnt9YHYLnI", key: "0", note: "유료:200개", genre: ["kpop","pay"] },
       { artist: "가비엔제이(Gavy NJ)", title: "낮과 밤(LOVE ALL)", inst: "", key: "0", note: "", genre: ["kpop"] },
       { artist: "가비엔제이(Gavy NJ)", title: "해바라기", inst: "", key: "0", note: "", genre: ["kpop"] },
+      { artist: "가을방학", title: "가끔 미치도록 네가 안고 싶어질 때가 있어", inst: "https://youtu.be/jeBi2T88UhU", key: "0", note: "", genre: ["kpop"] },
 ];
